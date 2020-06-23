e5622336f4fe66ac27ca7508330b78893ed2ff71
kiranaywt <56025572+kiranaywt@users.noreply.github.com>
2020-06-23 14:14:34 +0700

    Update config.php

--- application/config/config.php
+++ application/config/config.php
@@ -24,7 +24,7 @@ defined('BASEPATH') or exit('No direct script access allowed');
 |
 */
 
-$config['base_url'] = 'http://localhost/FrameWork_1C_SIAkademik/';
+$config['base_url'] = 'http://localhost/project_cadangan/';
 
 
 
